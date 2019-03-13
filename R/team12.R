ddff2ee3d365af916082ed00c9632e3ca870d9f0
zeruiz <zedray.chang@gmail.com>
2019-03-13 17:23:07 -0500

    Add function

---
+++ R/team12.R
@@ -0,0 +1,47 @@
+#' Map dataframe generator form Team 12.
+#'
+#' @param file URL to local path to the shape data.
+#' @param tolerance A number.
+#' @export
+#' @return The dataframe of longitude, latitude, group and order.
+#' @examples
+#' team_12("./data/gadm36_AUS_shp/gadm36_AUS_1.shp", 0.1)
+
+library(tidyverse)
+library(purrr)
+library(sf)
+
+helper <- function(d){
+  d <- unlist(d,recursive = FALSE)
+  d <- purrr::map(d,.f=add_order)
+  d <- add_layer(d)
+  return(d)
+}
+add_order <- function(d){
+  l <- nrow(d)
+  return(cbind(d,seq(1,l,by=1)))
+}
+add_layer <- function(d){
+  ll <- unlist(lapply(d,nrow))
+  d <- do.call(rbind,d)
+  d <- cbind(d,rep(c(1:length(ll)),time=ll))
+  return(d)
+}
+#purrr::map(oz$geometry, .f=helper) -> res
+#res <- add_layer(res)
+#colnames(res) <- c('long','lat','order','group','geo')
+#res <- as.data.frame(res)
+#ozbig <- read_sf("./data/gadm36_AUS_shp/gadm36_AUS_1.shp")
+#oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = 0.1, minarea = 0.001, topologyPreserve = TRUE)
+#oz <- st_as_sf(oz_st)
+
+team_12 <- function(file, tolerance){
+  ozbig <- read_sf(file)
+  oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance, minarea = 0.001, topologyPreserve = TRUE)
+  oz <- st_as_sf(oz_st)
+  purrr::map(oz$geometry, .f=helper) -> res
+  res <- add_layer(res)
+  colnames(res) <- c('long','lat','order','group','geo')
+  res <- as.data.frame(res)
+  res
+}
