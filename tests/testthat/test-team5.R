286b0efc3aaad7225ca692c711ac557bc5e50de9
floatgreen <yikelikeli@gmail.com>
2019-03-24 05:05:05 -0500

    update

--- tests/testthat/test-team5.R
+++ tests/testthat/test-team5.R
@@ -4,13 +4,12 @@ context("test-team5")
 
 # - Check parameters, first should be a string and second should be numeric
 # - Check if the path of file id correct
-# - Check if the function returns a dataframe
+
 
 
 test_that("team5 works as expected", {
   expect_error(team_5(./data/gadm36_AUS_shp/gadm36_AUS_1.shp, 0.1))
   expect_error(team_5("./data/gadm36_AUS_shp/gadm36_AUS_1.shp", tolerance))
   expect_error(team_5("gadm36_AUS_1.shp", 0,1))
-  #expect_true(is.data.frame(team_5("./data/gadm36_AUS_shp/gadm36_AUS_1.shp", 0.1)))
 })
 
