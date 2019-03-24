9ee9f1d5ed88d4c84fd43aaf57cf710aa3589280
zeruiz <zedray.chang@gmail.com>
2019-03-24 12:18:10 -0500

    Add function.

--- R/team12.R
+++ R/team12.R
@@ -7,26 +7,7 @@
 #' @examples
 #' team_12("./data/gadm36_AUS_shp/gadm36_AUS_1.shp", 0.1)
 
-library(tidyverse)
-library(purrr)
-library(sf)
 
-helper <- function(d){
-  d <- unlist(d,recursive = FALSE)
-  d <- purrr::map(d,.f=add_order)
-  d <- add_layer(d)
-  return(d)
-}
-add_order <- function(d){
-  l <- nrow(d)
-  return(cbind(d,seq(1,l,by=1)))
-}
-add_layer <- function(d){
-  ll <- unlist(lapply(d,nrow))
-  d <- do.call(rbind,d)
-  d <- cbind(d,rep(c(1:length(ll)),time=ll))
-  return(d)
-}
 #purrr::map(oz$geometry, .f=helper) -> res
 #res <- add_layer(res)
 #colnames(res) <- c('long','lat','order','group','geo')
@@ -36,9 +17,28 @@ add_layer <- function(d){
 #oz <- st_as_sf(oz_st)
 
 team_12 <- function(file, tolerance){
-  ozbig <- read_sf(file)
+  library(tidyverse)
+
+  add_order <- function(d){
+    l <- nrow(d)
+    return(cbind(d,seq(1,l,by=1)))
+  }
+  add_layer <- function(d){
+    ll <- unlist(lapply(d,nrow))
+    d <- do.call(rbind,d)
+    d <- cbind(d,rep(c(1:length(ll)),time=ll))
+    return(d)
+  }
+  helper <- function(d){
+    d <- unlist(d,recursive = FALSE)
+    d <- purrr::map(d,.f=add_order)
+    d <- add_layer(d)
+    return(d)
+  }
+
+  ozbig <- sf::read_sf(file)
   oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance, minarea = 0.001, topologyPreserve = TRUE)
-  oz <- st_as_sf(oz_st)
+  oz <- sf::st_as_sf(oz_st)
   purrr::map(oz$geometry, .f=helper) -> res
   res <- add_layer(res)
   colnames(res) <- c('long','lat','order','group','geo')
